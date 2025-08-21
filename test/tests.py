3b5bd948328d685d6db2e26269c59e3c8101a7c6
Mikhail <111HyherScool@kek.lol>
2025-08-22 00:12:58 +0300

    6 stage passed

--- test/tests.py
+++ test/tests.py
@@ -7,26 +7,26 @@ from .base import HyperSchoolTest
 class HyperSchoolTestRunner(HyperSchoolTest):
 
     funcs = [
-        # # 1 task
-        # HyperSchoolTest.check_create_courses,
-        # # 2 task
-        # HyperSchoolTest.check_main_page_header,
-        # HyperSchoolTest.check_main_search_courses,
-        # HyperSchoolTest.check_link_to_course,
-        # HyperSchoolTest.check_main_courses_count,
-        # # 3 task
-        # HyperSchoolTest.check_links_course_page,
-        # HyperSchoolTest.check_course_info_displayed,
-        # HyperSchoolTest.check_links_teacher_page,
-        # # 4 task
-        # HyperSchoolTest.check_link_add_to_course,
-        # # 5 task
-        # HyperSchoolTest.check_main_page_login_link,
-        # HyperSchoolTest.check_main_page_signup_link,
+        # 1 task
+        HyperSchoolTest.check_create_courses,
+        # 2 task
+        HyperSchoolTest.check_main_page_header,
+        HyperSchoolTest.check_main_search_courses,
+        HyperSchoolTest.check_link_to_course,
+        HyperSchoolTest.check_main_courses_count,
+        # 3 task
+        HyperSchoolTest.check_links_course_page,
+        HyperSchoolTest.check_course_info_displayed,
+        HyperSchoolTest.check_links_teacher_page,
+        # 4 task
+        HyperSchoolTest.check_link_add_to_course,
+        # 5 task
+        HyperSchoolTest.check_main_page_login_link,
+        HyperSchoolTest.check_main_page_signup_link,
         HyperSchoolTest.check_signup,
-        # HyperSchoolTest.check_login,
-        # # 6 task
-        # HyperSchoolTest.check_list_of_students
+        HyperSchoolTest.check_login,
+        # 6 task
+        HyperSchoolTest.check_list_of_students
 
     ]
 
